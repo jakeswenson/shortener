eee3e1d2b4e386be13c196494288d2bd457166da
Jake Swenson <jake@jakeswenson.com>
2024-11-30 09:28:16 -0800

    Update versions, and fight with wasm-bindgen issues...

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,23 @@ mod utils;
 
 fn log_request(req: &Request) {
   console_log!(
-        "{} - [{}], located at: {:?}, within: {}",
-        Date::now().to_string(),
-        req.path(),
-        req.cf().coordinates().unwrap_or_default(),
-        req.cf().region().unwrap_or("unknown region".into())
-    );
+    "{} - [{}], located at: {:?}, within: {}",
+    Date::now().to_string(),
+    req.path(),
+    req.cf().map(|c| c.colo()).unwrap_or_default(),
+    req
+      .cf()
+      .and_then(|c| c.country())
+      .unwrap_or("unknown region".into())
+  );
 }
 
 #[event(fetch)]
-pub async fn main(req: Request, env: Env) -> Result<Response> {
+pub async fn main(
+  req: Request,
+  env: Env,
+  _ctx: Context,
+) -> Result<Response> {
   log_request(&req);
 
   // Optionally, get more helpful error messages written to the console in the case of a panic.
@@ -22,23 +29,26 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
   // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
   // catch-alls to match on specific patterns. The Router takes some data with its `new` method
   // that can be shared throughout all routes. If you don't need any shared data, use `()`.
-  let router = Router::new(());
+  let router = Router::new();
 
   // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
   // functionality and a `RouteContext` which you can use to  and get route parameters and
   // Enviornment bindings like KV Stores, Durable Objects, Secrets, and Variables.
   let result = router
-    .get_async("/:url", |mut _req, ctx| async move {
+    .get_async("/", |_req, ctx| async move {
+      let mut headers = Headers::new();
+      let home_redirect = ctx.var("HOME")?;
+      headers.append("Location", &home_redirect.to_string())?;
+      Response::empty().map(|r| r.with_headers(headers).with_status(301))
+    })
+    .get_async("/:url", |_req, ctx| async move {
       console_log!("Looking for url");
 
       if let Some(name) = ctx.param("url") {
         console_log!("Looking for url: {}", name);
 
-        if let Some(value) = ctx.kv("urls")?.get(name).await? {
+        if let Some(location) = ctx.kv("urls")?.get(name).text().await? {
           console_log!("Found KV for url: {}", name);
-
-          let location = value.as_string();
-
           console_log!("Redirecting to: {}", location);
 
           let mut headers = Headers::new();
@@ -46,8 +56,7 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
           headers.append("Location", &location)?;
 
           console_log!("Responding...");
-          Response::empty()
-            .map(|r| r.with_headers(headers).with_status(301))
+          Response::empty().map(|r| r.with_headers(headers).with_status(301))
         } else {
           console_log!("No url for: {}", name);
           Response::error("Requested url not found", 404)
--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,9 @@
-use cfg_if::cfg_if;
+pub fn set_panic_hook() {
+  // https://github.com/rustwasm/console_error_panic_hook#readme
 
-cfg_if! {
-    // https://github.com/rustwasm/console_error_panic_hook#readme
-    if #[cfg(feature = "console_error_panic_hook")] {
-        extern crate console_error_panic_hook;
-        pub use self::console_error_panic_hook::set_once as set_panic_hook;
-    } else {
-        #[inline]
-        pub fn set_panic_hook() {}
-    }
+  #[cfg(feature = "debug")]
+  {
+    pub use console_error_panic_hook::set_once;
+    set_once();
+  }
 }
